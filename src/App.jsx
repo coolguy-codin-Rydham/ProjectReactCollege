6266ba0df337a21007eae980127995dcf2898caa
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-05-09 19:08:08 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+ return (
+  <div>
+    <div className="bg-gray-800 py-8 text-white flex items-center justify-center">
+      <main className=" max-w-[1440px] h-full w-full flex items-center justify-center"  id="home">
+        Hello
+
+      </main>
+    </div>
+  </div>
+ )
+}
+
+export default App
